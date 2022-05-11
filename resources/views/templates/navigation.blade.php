de6f3bf8c920759c749760cd0cd71c53de30bfbd
satosat <satriotimotius@gmail.com>
2022-05-11 19:33:48 +0700

    pert 2

--- resources/views/templates/navigation.blade.php
+++ resources/views/templates/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/books">Demo</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Demo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,14 +8,14 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/books">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Books
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/books/create">Create New Book</a></li>
+                        <li><a class="dropdown-item" href="{{ route('create_book') }}">Create New Book</a></li>
                     </ul>
                 </li>
             </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return redirect('/books');
+// });
+
+Route::redirect('/', '/books');
+
+// Route::get('/banned', function () {
+//     return abort(500);
+// });
 
 Route::get('/books', function () {
     return view('book.index');
-});
+})->name('home');
 
 Route::get('/books/create', function () {
     return view('book.create');
-});
+})->name('create_book');
 
 require __DIR__.'/auth.php';
